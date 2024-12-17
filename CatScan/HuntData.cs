908dbbfa5da705ab86141124864e2e94ce79d8df
Senko <152493636+helpful-fox-senko-san@users.noreply.github.com>
2024-12-17 11:16:53 +0000

    Patch 7.15 data, CatScan v0.9.4.0

--- CatScan/HuntData.cs
+++ CatScan/HuntData.cs
@@ -281,16 +281,16 @@ public static partial class HuntData
         {1187, DT_Zone("Urqopacha", 1,
             B("Mad Maguey"), B("Chupacabra"), A("Nechuciho"), A("Queen Hawk"), S("Kirlirger the Abhorrent")
         )},
-        {1188, DT_Zone("Kozama'uka", 3,
+        {1188, DT_Zone("Kozama'uka", 1,
             B("The Slammer"), B("Go'ozoabek'be"), A("Pkuucha"), A("The Raintriller"), S("Ihnuxokiy")
         )},
         {1189, DT_Zone("Yak T'el", 1,
             B("Leafsource Hadoll Ja"), B("Xty'iinbek"), A("Starcrier"), A("Rrax Yity'a"), S("Neyoozoteel")
         )},
-        {1190, DT_Zone("Shaaloani", 2,
+        {1190, DT_Zone("Shaaloani", 1,
             B("Nopalitender Fabuloso"), B("Uktena"), A("Yehehetoaua'pyo"), A("Keheniheyamewi"), S("Sansheya"), FATE("Ttokrrone")
         )},
-        {1191, DT_Zone("Heritage Found", 2,
+        {1191, DT_Zone("Heritage Found", 1,
             B("Gallowsbeak"), B("Gargant"), A("Heshuala"), A("Urna Variabilis"), S("Atticus the Primogenitor")
         )},
         {1192, DT_Zone("Living Memory", 1,
